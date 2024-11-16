10f5a934904210292a593c138daba63a818eb368
André Vale <andre_ramosdovale@outlook.com>
2024-11-16 18:21:13 -0300

    feat: finalização navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <head></head>
-      <body>
+      <body className='bg-gray-800 text-white'>
         <Navbar />
         <main>{children}</main>
       </body>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,17 +9,17 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-white shadow-md">
+    <nav className="bg-gray-900 shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         {/* Logo da empresa */}
         <div className="flex items-center">
-          <Image src={Logo} alt="Logo da Empresa" className="h-12 w-auto" />
-          <h3 className="text-gray-800 ml-2">Hive.Connect</h3>
+          <Image src={Logo} alt="Logo da Empresa" className="h-14 w-auto" />
+          <h3 className="ml-2">Hive.Connect</h3>
         </div>
 
         {/* Botão de menu hamburguer para mobile */}
         <div className="md:hidden">
-          <button onClick={() => setIsOpen(!isOpen)} className="text-gray-800 focus:outline-none">
+          <button onClick={() => setIsOpen(!isOpen)} className="focus:outline-none">
             {/* Alterna entre o ícone de menu e o ícone de fechar */}
             {isOpen ? <X className="w-6 h-6" /> : <Menu className="w-6 h-6" />}
           </button>
@@ -27,9 +27,9 @@ export default function Navbar() {
 
         {/* Links de navegação - visíveis apenas no desktop */}
         <div className="hidden md:flex space-x-8">
-          <Link href="/" className="text-gray-800 hover:text-blue-500"> Home </Link>
-          <Link href="/quem-somos" className="text-gray-800 hover:text-blue-500"> Quem Somos </Link>
-          <Link href="/contato" className="text-gray-800 hover:text-blue-500"> Contato </Link>
+          <Link href="/" className="hover:bg-gradient-to-r from-customColors-color4 via-customColors-color5 to-customColors-color6 hover:text-transparent hover:bg-clip-text"> Home </Link>
+          <Link href="/quem-somos" className="hover:bg-gradient-to-r from-customColors-color4 via-customColors-color5 to-customColors-color6 hover:text-transparent hover:bg-clip-text"> Quem Somos </Link>
+          <Link href="/contato" className="hover:bg-gradient-to-r from-customColors-color4 via-customColors-color5 to-customColors-color6 hover:text-transparent hover:bg-clip-text"> Contato </Link>
         </div>
       </div>
 
@@ -37,9 +37,9 @@ export default function Navbar() {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1">
-            <Link href="/" className="text-gray-800 hover:text-blue-500 block"> Home </Link>
-            <Link href="/quem-somos" className="text-gray-800 hover:text-blue-500 block"> Quem Somos </Link>
-            <Link href="/contato" className="text-gray-800 hover:text-blue-500 block"> Contato </Link>
+            <Link href="/" className=" hover:text-blue-500 block"> Home </Link>
+            <Link href="/quem-somos" className=" hover:text-blue-500 block"> Quem Somos </Link>
+            <Link href="/contato" className=" hover:text-blue-500 block"> Contato </Link>
           </div>
         </div>
       )}
